e9f256f4278ae3dc3fc1890195a23454d732efd7
eric <libra9z@163.com>
2017-01-04 09:06:05 +0800

    增加部分命令行参数

--- root.go
+++ root.go
@@ -10,9 +10,15 @@ import (
 var RootCmd *cobra.Command
 
 var(
-	Address	string
-	Consul	string
-	Token	string
+	Address			string
+	Consul			string
+	Token			string
+	Debug			bool
+	DebugAddr		string
+	HttpAddr		string
+	ZipkinAddr		string
+	AppdashAddr		string
+	LightstepToken	string
 )
 
 func init() {
@@ -21,6 +27,13 @@ func init() {
 	RootCmd.PersistentFlags().StringVar(&Address, "addr", "", "config this service address format(ip:port).")
 	RootCmd.PersistentFlags().StringVar(&Consul, "consul", "", "config consul address format(ip:port).")
 	RootCmd.PersistentFlags().StringVar(&Token, "token", "", "config consul acl token.")
+
+	RootCmd.PersistentFlags().BoolVar(&Debug,"debug", false, "enable debug mode true or false")
+	RootCmd.PersistentFlags().StringVar(&DebugAddr,"debug.addr", ":8080", "Debug and metrics listen address")
+	RootCmd.PersistentFlags().StringVar(&HttpAddr,"http.addr", ":8081", "HTTP listen address")
+	RootCmd.PersistentFlags().StringVar(&ZipkinAddr,"zipkin.addr", "", "Enable Zipkin tracing via a Kafka server host:port")
+	RootCmd.PersistentFlags().StringVar(&AppdashAddr,"appdash.addr", "", "Enable Appdash tracing via an Appdash server host:port")
+	RootCmd.PersistentFlags().StringVar(&LightstepToken,"lightstep.token", "", "Enable LightStep tracing via a LightStep access token")
 
 	RootCmd.AddCommand(versionCmd)
 }
